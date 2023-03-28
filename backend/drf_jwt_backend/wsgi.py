66a6fd211799310a07f59e520d05be6b4c4ee900
aamrlyman <aamrlyman@gmail.com>
2023-03-27 18:42:40 -0700

    move wsgi application into a new file

--- backend/drf_jwt_backend/wsgi.py
+++ backend/drf_jwt_backend/wsgi.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'drf_jwt_backend.settings')
 
-application = get_wsgi_application()
+
