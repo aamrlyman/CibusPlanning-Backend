5b35424b6520c2fa65d92e37a405f1f88494e2e6
aamrlyman <aamrlyman@gmail.com>
2023-03-27 19:08:40 -0700

    put application back in wsgi file

--- backend/drf_jwt_backend/wsgi.py
+++ backend/drf_jwt_backend/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'drf_jwt_backend.settings')
 
-
+application = get_wsgi_application()
